bbf7038e5135b99ab1b51159b956ff14b73f7046
Théo <theo.boldireff@edu.devinci.fr>
2020-10-03 19:54:02 +0200

    Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public float timer;
+    private float initTimer;
+    public Text timerText;
+
     public Transform respawnPoint;
     public GameObject player;
     public GameObject deadPlayer;
+
+    private void Start()
+    {
+        initTimer = timer;
+        StartCoroutine(Timer());
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        timerText.text = "0 : " + timer.ToString("F0");
+    }
+
     public void Respawn()
     {
         var dead = Instantiate(deadPlayer);
         dead.transform.position = player.transform.position;
         player.transform.position = respawnPoint.position;
+        timer = initTimer;
+        StartCoroutine(Timer());
     }
 
     public void Die()
     {
         player.transform.position = respawnPoint.position;
+        timer = initTimer;
     }
 
     public void Reset()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+
+    public IEnumerator Timer()
+    {
+        yield return new WaitUntil(() => timer <= 0);
+        Respawn();
     }
 }
