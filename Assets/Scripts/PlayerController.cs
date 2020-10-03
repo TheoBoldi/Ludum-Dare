f5097e4351d458baedce889d68bf7fc7889a171d
Théo <theo.boldireff@edu.devinci.fr>
2020-10-03 18:16:53 +0200

    Player controller v1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string playerName;
+
+    public PlayerEntity entity;
+    private Player _mainPlayer;
+
     void Start()
     {
-        
+        _mainPlayer = ReInput.players.GetPlayer(playerName);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float dirX = _mainPlayer.GetAxis("MoveHorizontal");
+        if (dirX != 0f)
+        {
+            dirX = Mathf.Sign(dirX);
+        }
+        entity.Move(dirX);
+
+        if (_mainPlayer.GetButtonDown("Jump") && entity.IsOnground())
+        {
+            entity.Jump();
+        }
+        if (_mainPlayer.GetButtonUp("Jump") && entity.IsJumping())
+        {
+            entity.StopJump();
+        }
     }
 }
